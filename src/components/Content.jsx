81932a4529c5731906223e856910d9868565e4fe
EvyatarHaim1 <evyataros1@gmail.com>
2022-06-25 11:40:48 +0300

    finish the UI of the forth screen

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box } from '@chakra-ui/react';
 
 import { NoReport } from './NoReport';
@@ -7,14 +7,20 @@ import { AllProjectsGatewayOne } from './AllProjectsGatewayOne';
 import { ProjectOneAllGateways } from './ProjectOneAllGateways';
 import { ProjectOneGatewayOne } from './ProjectOneGatewayOne';
 
-export const Content = () => (
-  <Box {...ScrollStyle}>
-    {/* <NoReport /> */}
-    {/* <AllProjectsAllGateways /> */}
-    <AllProjectsGatewayOne />
-    {/* <ProjectOneAllGateways /> */}
-    {/* {/* <ProjectOneGatewayOne /> */}
-  </Box>
-);
+export const Content = () => {
+  const [currentScreen, setCurrentScreen] = useState('ProjectOneGatewayOne');
 
-const ScrollStyle = {};
+  const renderScreen = () => {
+    if (currentScreen === 'AllProjectsAllGateways')
+      return <AllProjectsAllGateways />;
+    else if (currentScreen === 'AllProjectsGatewayOne')
+      return <AllProjectsGatewayOne />;
+    else if (currentScreen === 'ProjectOneAllGateways')
+      return <ProjectOneAllGateways />;
+    else if (currentScreen === 'ProjectOneGatewayOne')
+      return <ProjectOneGatewayOne />;
+    else return <NoReport />;
+  };
+
+  return <Box>{renderScreen()}</Box>;
+};
--- src/components/ProjectOneGatewayOne.jsx
+++ src/components/ProjectOneGatewayOne.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
-import { Box, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Text } from '@chakra-ui/react';
 import { messages } from '../messages';
+import { TableRow } from './common/TableRow';
 
 export const ProjectOneGatewayOne = () => (
-  <Box>
-    <Heading>{messages.titles.projectOneGatewayOne}</Heading>
-  </Box>
+  <>
+    <Flex {...ContainerStyle}>
+      <Heading {...TitleStyle}>{messages.titles.projectOneGatewayOne}</Heading>
+      <TableRow
+        gatewayOne
+        columns={[
+          messages.paragraphs.date,
+          messages.paragraphs.transactionID,
+          messages.paragraphs.amount,
+        ]}
+      />
+      <TableRow
+        bgColor="lightBlue.100"
+        columns={[
+          messages.paragraphs.date2,
+          messages.paragraphs.transactionExp,
+          messages.paragraphs.amount2,
+        ]}
+      />
+      <TableRow
+        columns={[
+          messages.paragraphs.date3,
+          messages.paragraphs.transactionExp,
+          messages.paragraphs.amount3,
+        ]}
+      />
+      <TableRow
+        bgColor="lightBlue.100"
+        columns={[
+          messages.paragraphs.date4,
+          messages.paragraphs.transactionExp,
+          messages.paragraphs.amount4,
+        ]}
+      />
+    </Flex>
+    <Text {...ProjectTitleStyle}>{messages.paragraphs.totalBottom}</Text>
+  </>
 );
+
+const ContainerStyle = {
+  flexDirection: 'column',
+  bgColor: 'lightBlue.100',
+  p: '18px 27px 17px 19px',
+  my: '27px',
+  borderRadius: '10px',
+};
+
+const TitleStyle = {
+  fontSize: '16px',
+  fontWeight: 'bold',
+  p: '18px 0px 34px 24px',
+};
+
+const ProjectTitleStyle = {
+  h: '53px',
+  borderRadius: '10px',
+  p: '17px 0px 17px 19px',
+  mb: '50px',
+  fontWeight: 'bold',
+  color: 'blue.500',
+  bgColor: 'lightBlue.100',
+};
